6e872d7f9fd20aea1ed69ba43aae2a18d58be2fd
Daniel <danieldcc.colares@gmail.com>
2025-11-08 10:48:31 -0300

    adiciona componente Header

---
+++ src/components/main-header.tsx
@@ -0,0 +1,27 @@
+import type { ComponentProps } from "react";
+import Container from "./container";
+import LogoImg from "../assets/images/galeria-plus-full-logo.svg?react";
+import { Link } from "react-router";
+import cx from "classnames";
+import Button from "./button";
+
+interface MainHeaderProps extends ComponentProps<typeof Container> {}
+
+export default function MainHeader({ className, ...props }: MainHeaderProps) {
+  return (
+    <Container
+      as="header"
+      className={cx("flex justify-between items-center gap-10", className)}
+      {...props}
+    >
+      <Link to="/">
+        <LogoImg className="h-5" />
+      </Link>
+
+      <div className="flex items-center gap-3">
+        <Button>Nova foto</Button>
+        <Button variant="secondary">Criar álbum</Button>
+      </div>
+    </Container>
+  );
+}
