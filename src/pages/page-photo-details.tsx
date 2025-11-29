8da20cf6f95c8f020a41fec7074c054a7908779f
Daniel <danieldcc.colares@gmail.com>
2025-11-29 16:28:27 -0300

    adiciona página de detalhes - Lista de álbuns

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -6,6 +6,7 @@ import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 
 export default function PagePhotoDetails() {
   const { id } = useParams<{ id: string }>();
@@ -22,6 +23,7 @@ export default function PagePhotoDetails() {
       { id: "3423", title: "Album 3" },
     ],
   } as Photo;
+
   return (
     <Container>
       <header className="flex items-center justify-between gap-8 mb-8">
@@ -34,7 +36,7 @@ export default function PagePhotoDetails() {
         <PhotosNavigator />
       </header>
 
-      <div className="grid grid-cols-[21rem] gap-24">
+      <div className="grid grid-cols-[21rem_1fr] gap-24">
         <div className="space-y-3">
           {!isLoadingPhoto ? (
             <ImagePreview
@@ -51,6 +53,22 @@ export default function PagePhotoDetails() {
           ) : (
             <Skeleton className="w-20 h-10" />
           )}
+        </div>
+
+        <div className="py-3">
+          <Text as="h3" variant="heading-medium" className="mb-6">
+            Álbuns
+          </Text>
+
+          <AlbumsListSelectable
+            photo={photo}
+            albums={[
+              { id: "3421", title: "Album 1" },
+              { id: "3422", title: "Album 2" },
+              { id: "3423", title: "Album 3" },
+            ]}
+            loading={isLoadingPhoto}
+          />
         </div>
       </div>
     </Container>
