037863250f328f3e750b3449a44bedc3ab5a956d
Daniel <danieldcc.colares@gmail.com>
2025-11-29 15:22:06 -0300

    adiciona página de detalhes - mostrar imagem

---
+++ src/contexts/photos/components/photos-navigator.tsx
@@ -0,0 +1,55 @@
+import type { ComponentProps } from "react";
+import Skeleton from "../../../components/skeleton";
+import ButtonIcon from "../../../components/button-icon";
+import ArrowLeftIcon from "../../../assets/icons/chevron-left.svg?react";
+import ArrowRightIcon from "../../../assets/icons/chevron-right.svg?react";
+import Button from "../../../components/button";
+import { useNavigate } from "react-router";
+import cx from "classnames";
+
+interface PhotosNavigatorProps extends ComponentProps<"div"> {
+  previousPhotoId?: string;
+  nextPhotoId?: string;
+  loading?: boolean;
+}
+
+export default function PhotosNavigator({
+  loading,
+  nextPhotoId,
+  previousPhotoId,
+  className,
+  ...props
+}: PhotosNavigatorProps) {
+  const navigate = useNavigate();
+  return (
+    <div className={cx("flex gap-2", className)} {...props}>
+      {!loading ? (
+        <>
+          <ButtonIcon
+            icon={ArrowLeftIcon}
+            variant="secondary"
+            disabled={!previousPhotoId}
+            onClick={() => {
+              navigate(`/fotos/${previousPhotoId}`);
+            }}
+          />
+          <Button
+            icon={ArrowRightIcon}
+            variant="secondary"
+            disabled={!nextPhotoId}
+            onClick={() => {
+              navigate(`/fotos/${nextPhotoId}`);
+            }}
+          >
+            Próxima Imagem
+          </Button>
+        </>
+      ) : (
+        <>
+          <Skeleton className="w-10 h-10" />
+          <Skeleton className="w-20 h-10" />
+        </>
+      )}
+    </div>
+  );
+}
--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,13 +1,58 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
   const { id } = useParams<{ id: string }>();
+
+  //Para teste de mock
+  const isLoadingPhoto = false;
+  const photo = {
+    id: "123",
+    title: "olá mundo!",
+    imageId: "portrait-tower.png",
+    albums: [
+      { id: "3421", title: "Album 1" },
+      { id: "3422", title: "Album 2" },
+      { id: "3423", title: "Album 3" },
+    ],
+  } as Photo;
   return (
-    <>
-      <Text variant="heading-medium">Pagina detalhe da foto</Text>
-      <hr />
-      <Text variant="heading-medium">ID da foto: {id}</Text>
-    </>
+    <Container>
+      <header className="flex items-center justify-between gap-8 mb-8">
+        {!isLoadingPhoto ? (
+          <Text variant="heading-large">{photo?.title}</Text>
+        ) : (
+          <Skeleton className="w-48 h-8" />
+        )}
+
+        <PhotosNavigator />
+      </header>
+
+      <div className="grid grid-cols-[21rem] gap-24">
+        <div className="space-y-3">
+          {!isLoadingPhoto ? (
+            <ImagePreview
+              src={`/images/${photo?.imageId}`}
+              title={photo?.title}
+              imageClassName="h-[21rem]"
+            />
+          ) : (
+            <Skeleton className="h-[21rem]" />
+          )}
+
+          {!isLoadingPhoto ? (
+            <Button variant="destructive">Excluir</Button>
+          ) : (
+            <Skeleton className="w-20 h-10" />
+          )}
+        </div>
+      </div>
+    </Container>
   );
 }
