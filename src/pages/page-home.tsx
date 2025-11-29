5ab5a7ddfc2d881d8434bef58802f9df7dea86fc
Daniel <danieldcc.colares@gmail.com>
2025-11-29 12:38:00 -0300

    adiciona mock filtro de álbuns

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,11 +1,43 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 
 import PhotosList from "../contexts/photos/components/photos-list";
 
 export default function PageHome() {
   return (
     <Container>
-      <PhotosList photos={[]} loading />
+      <AlbumsFilter
+        albums={[
+          { id: "3421", title: "Album 1" },
+          { id: "3422", title: "Album 2" },
+          { id: "3423", title: "Album 3" },
+        ]}
+        className="mb-9"
+      />
+      <PhotosList
+        photos={[
+          {
+            id: "123",
+            title: "olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "3422", title: "Album 2" },
+              { id: "3423", title: "Album 3" },
+            ],
+          },
+          {
+            id: "123",
+            title: "olá mundo!",
+            imageId: "portrait-tower.png",
+            albums: [
+              { id: "3421", title: "Album 1" },
+              { id: "3422", title: "Album 2" },
+              { id: "3423", title: "Album 3" },
+            ],
+          },
+        ]}
+      />
     </Container>
   );
 }
